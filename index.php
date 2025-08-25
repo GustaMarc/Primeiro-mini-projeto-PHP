8674ec1578a0d38971d327918e34643fa4cdbc2a
Gustavo Marcondes <157168456+GustaMarc@users.noreply.github.com>
2025-08-24 21:44:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+$carro= "<h1>Carros</h1>";
+
+$marca="Marca: Ferrari";
+$modelo="Modelo: 812 gtb";
+$ano="Ano: 2017";
+$valor="Valor: R$6000.000";
+
+
+$time="<h1>Time de futebol</h1>";
+
+$nome="Nome: Corinthians";
+$ano_criacao="Ano de criação: 1910";
+$tecnico_atual="Técnico atual: Dorival Júnior";
+$titulos="Títulos oficiais: 54";
+
+$funcionario="<h1>Funcionário</h1>";
+
+$nome_func= "Nome: Jorge";
+$salario="Salário; 8000";
+$idade="Idade: 30";
+$cargo="Cargo: Analista de Dados Pleno";
+
+echo"$carro <br> $marca <br> $modelo <br> $ano <br> $valor <br>";
+echo"$time <br> $nome <br> $ano_criacao <br> $tecnico_atual <br> $titulos <br>";
+echo"$funcionario <br> $nome_func <br> $salario <br> $idade <br> $cargo <br>";
+?>
